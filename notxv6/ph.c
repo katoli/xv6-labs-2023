e735a521acf3802436bd36142c47e6fc9a02d3ff
katoli <1509525007@qq.com>
2024-01-18 03:05:31 +0800

    modified:   notxv6/barrier.c 	modified:   notxv6/ph.c 	modified:   user/uthread.c 	modified:   user/uthread_switch.S 	time.txt

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t bucket_lock[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -40,7 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&bucket_lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,14 +55,13 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&bucket_lock[i]);
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -116,6 +116,10 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for (int i = 0;i < NBUCKET; i++){
+    pthread_mutex_init(&bucket_lock[i], NULL);
   }
 
   //
